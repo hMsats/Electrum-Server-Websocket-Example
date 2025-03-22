b91b236501b5407f8a3f0a1ca1619c8e6fc40371
hMsats <millibitcoins@gmail.com>
2025-03-22 15:57:52 +0100

    websocket.js not in readme

---
+++ websocket.js
@@ -0,0 +1,34 @@
+// For the client:
+// sudo apt install nodejs
+// sudo apt install npm
+// npm i ws
+// Run on client as:
+// node websocket.js
+
+// For the server:
+// sudo ufw allow 50003
+// sudo ufw allow 50004
+// On the modem:
+// open port 50003
+// open port 50004
+
+const WebSocket = require('ws');
+const socket = new WebSocket("wss://bitcoinserver.nl:50004");
+//const socket = new WebSocket("ws://bitcoinserver.nl:50003");
+
+// Connection opened
+socket.addEventListener("open", () => {
+  console.log("Connection opened");
+});
+
+socket.addEventListener("close", () => {
+  console.log("Connection closed");
+});
+
+socket.addEventListener("error", e => {
+  console.log("Error\n\n", e);
+});
+
+socket.addEventListener("message", function(event) {
+  console.log("Message from server ", event.data);
+});
